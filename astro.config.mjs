edc1925fbcf541d0e15949b6599cc68ede642f40
Dev <dev@harryadney.com>
2025-09-08 22:56:17 +0100

    Fix links to internal pages Fix path to images on history page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import photostream from 'astro-photostream';
+// import photostream from 'astro-photostream';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), photostream()],
+  integrations: [tailwind()], // Temporarily disabled photostream(),
   output: 'static',
   build: {
     assets: 'assets'
